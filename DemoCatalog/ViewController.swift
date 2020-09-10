100e7bdf80256028830d53dff6b46f2718ec1ebe
dshik <dshik@mail.ru>
2020-09-10 10:29:00 +0300

    Начинаем делать демо

---
+++ DemoCatalog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoCatalog
+//
+//  Created by Дмитрий Шикунов on 10.09.2020.
+//  Copyright © 2020 Дмитрий Шикунов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
